3da4f5626e04925cefb6fe56a5da5a010d5c5b29
seaniiio <cnc4750@naver.com>
2024-07-02 14:14:52 +0900

    feat: #4 회원가입 수정 -> user, protector, address 합친 serializer 사용하도록 수정

--- taxi_project/myAPP/serializer.py
+++ taxi_project/myAPP/serializer.py
@@ -11,10 +11,40 @@ class ProtectorSerializer(serializers.ModelSerializer):
         model = Protector
         fields = '__all__'
 
+class ProtectorRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Protector
+        fields = ['protector_name', 'protector_phone']
+
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = Address
         fields = '__all__'
+
+class AddressRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['address_name', 'road_address', 'detail_address', 'latitude', 'longitude']
+
+class UserRegisterSerializer(serializers.Serializer):
+    user = UserSerializer()
+    protector = ProtectorRegisterSerializer()
+    address = AddressRegisterSerializer()
+
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+        protector_data = validated_data.pop('protector')
+        address_data = validated_data.pop('address')
+
+        user = User.objects.create(**user_data)
+        user.set_password(user_data['user_pwd'])
+        user.save()
+
+        Protector.objects.create(user_id=user, **protector_data)
+        Address.objects.create(user_id=user, **address_data)
+       
+        return user
+
 
 class UserInfoSerializer(serializers.ModelSerializer):
     class Meta:
--- taxi_project/myAPP/views.py
+++ taxi_project/myAPP/views.py
@@ -9,48 +9,27 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from drf_yasg.utils import swagger_auto_schema
 
 from .models import *
 from .serializer import *
 
+@swagger_auto_schema(
+        method="POST", 
+        tags=["회원 api"],
+        operation_summary="회원가입", 
+        operation_description="회원가입을 진행합니다.",
+        request_body=UserRegisterSerializer
+)
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def signup(request):
+    serializer = UserRegisterSerializer(data = request.data)
 
-    user_data = request.data.get('user')
-    protector_data = request.data.get('protector')
-    address_data = request.data.get('address')
-
-    # User 데이터 저장
-    user_serializer = UserSerializer(data=user_data)
-    if user_serializer.is_valid():
-        user = user_serializer.save()
-        user.set_password(user_data['user_pwd'])
-        user.save()
-    else:
-        return Response({'status':'400','message':user_serializer.errors}, status=400)
-
-    # Protector 데이터 저장 (유저와 연결)
-    protector_data['user_id'] = user.id  # 유저 ID를 연결
-    protector_serializer = ProtectorSerializer(data=protector_data)
-    if protector_serializer.is_valid():
-        protector_serializer.save()
-    else:
-        return Response({'status':'400','message':protector_serializer.errors}, status=400)
-
-    # Address 데이터 저장 (유저와 연결)
-
-    #지금 이렇게 작성해두긴 했는데, 위도, 경도는 입력받은 도로명주소로 지도 api 호출해서 리턴받은 후 작성해야될듯. 
-    
-    address_data['user_id'] = user.id  # 유저 ID를 연결
-    address_serializer = AddressSerializer(data=address_data)
-    if address_serializer.is_valid():
-        address_serializer.save()
-    else:
-        return Response({'status':'400','message':address_serializer.errors}, status=400)
-
-    return Response({'status':'201','message': 'All data added successfully'}, status=201)
-
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'status':'201','message': 'All data added successfully'}, status=201)
+    return Response({'status':'400','message':serializer.errors}, status=400)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
