4850aa09e24ed7fea4ce57a1673c93475be726d3
seaniiio <cnc4750@naver.com>
2024-07-02 20:14:57 +0900

    feat #4: 위도, 경도 변환 api 구현

--- taxi_project/myAPP/views.py
+++ taxi_project/myAPP/views.py
@@ -12,8 +12,30 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
+from pathlib import Path
+from django.core.exceptions import ImproperlyConfigured
+
+import requests
+import json
 from .models import *
 from .serializer import *
+
+# secret.json에서 카카오 키 가져오기
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+secret_file = BASE_DIR / 'secrets.json'
+
+with open(secret_file) as file:
+    secrets = json.loads(file.read())
+
+def get_secret(setting,secrets_dict = secrets):
+    try:
+        return secrets_dict[setting]
+    except KeyError:
+        error_msg = f'Set the {setting} environment variable'
+        raise ImproperlyConfigured(error_msg)
+
+KAKAO_KEY = get_secret('KAKAO_KEY') 
 
 @swagger_auto_schema(
         method="POST", 
@@ -76,4 +98,32 @@ def new_taxi(request):
     if serializer.is_valid():
         serializer.save()
         return Response({'status':'201','message': 'All data added successfully'}, status=201)
+    return Response({'status':'400','message':serializer.errors}, status=400)
+
+
+# 도로명주소 -> 위,경도 변환 api
+@swagger_auto_schema(
+    method="POST", 
+    tags=["주소 api"],
+    operation_summary="위도, 경도 변환", 
+    request_body = RoadAddressSerializer
+)
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def coordinate(request):
+    #road_address = request.data.get('road_address')
+    serializer = RoadAddressSerializer(data = request.data)
+    # api 호출
+
+    if serializer.is_valid():
+        road_address = serializer.data['road_address']
+
+        url = 'https://dapi.kakao.com/v2/local/search/address?query={address}'.format(address=road_address)  # 요청을 보낼 API의 URL
+        headers = {
+            'Authorization': 'KakaoAK {}'.format(KAKAO_KEY),  # Authorization 헤더
+            'Content-Type': 'application/json'  # 필요에 따라 Content-Type 헤더 추가
+        }
+
+        result = json.loads(str(requests.get(url, headers=headers).text))
+        return Response({"road_address":road_address, "latitude":result["documents"][0]['y'], "longitude":result["documents"][0]['x']}, status=201)
     return Response({'status':'400','message':serializer.errors}, status=400)
