c55282d10c2e14058c163685a8489e51bced0b5e
sae2say <lollol0617@gmail.com>
2024-07-03 15:22:59 +0900

    feature: settings.py에 기본 메일 정보 등록

--- taxi_project/myAPP/views.py
+++ taxi_project/myAPP/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import update_last_login
 from django.http import JsonResponse, HttpResponse
+from django.core.mail import send_mail
 
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
