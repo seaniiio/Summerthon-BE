a8f6a30daa3d4774b5f9b63adc63ad37b3bd9d9b
seaniiio <cnc4750@naver.com>
2024-07-04 17:24:40 +0900

    feat: collectstatic

--- taxi_project/taxi_project/urls.py
+++ taxi_project/taxi_project/urls.py
@@ -5,6 +5,9 @@ from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 schema_view = get_schema_view(
     openapi.Info(
         title="taxi-project",
@@ -23,4 +26,4 @@ urlpatterns = [
     # Swagger url
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
